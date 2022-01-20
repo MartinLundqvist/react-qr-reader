f3a09864228630cfc83ab25297a5bc0c589f8869
MartinLundqvist <iphonelynden@gmail.com>
2022-01-20 21:22:58 +0100

    fixed layout issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ function App() {
   const [scanning, setScanning] = useState(false);
   return (
     <div className='container'>
-      {!scanning && <button onClick={() => setScanning(true)}>Scanna</button>}
+      {!scanning && (
+        <button onClick={() => setScanning(true)}>Percy's knapp</button>
+      )}
       {scanning && (
         <>
           <button onClick={() => setScanning(false)}>Stäng scanner</button>
 
           <BarcodeScannerComponent
-            width={500}
-            height={500}
+            width={300}
+            height={300}
             onUpdate={(err, result) =>
               result ? setData(result.getText()) : setData('Söker...')
             }
           />
 
-          <h3>Scannar och hittar</h3>
-          <p>{data}</p>
+          <h3>Resultat (filma en QR kod!!)</h3>
+          <div className='results__container'>{data}</div>
         </>
       )}
     </div>
